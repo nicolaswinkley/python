fdc30a213c89cd9cbe204e873dcbfeaa3f33301a
Nicolas Winkley <nwinkley@gmail.com>
2020-05-14 11:35:33 -0700

    Strings lecture commit

---
+++ Lectures/HelloWorld.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+print("Welcome to Python Scripting")
+print("This is my first script")
---
+++ Lectures/Strings.py
@@ -0,0 +1,21 @@
+# String in double or single quotes, prefer double
+# strings are ordered sequences
+# can use indexing and slicing to grab sub-sections of the string
+# indexing notation uses [] after the string (or variable assigned the string)
+# indexing allows you to grab a single character from the string
+
+# These actions use square brackets and a number index to indicate positions of what you wish to grab
+# Character:    h e l l o
+# Index:        0 1 2 3 4 
+# Reverse Index:0 -4 -3 -2 -1
+
+# Slicing allows you to grab a subsection of multiple characters, a "slice" of the string.
+# Syntax: [start:stop:step]
+# start is a numerical index for the slice start
+# stop is the index you will go up to (but not include) 
+# step is the size of the "jump" you will take
+
+'hello'
+"world"
+"This is also a string"
+"I'm going on a run"
